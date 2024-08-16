847a4b1a72af64934453a52bb5ec9d8207a3af93
leniero <leandronieroml@gmail.com>
2024-08-16 23:49:47 +0100

    page styling

--- main/static/main/nav-animation.js
+++ main/static/main/nav-animation.js
@@ -1,5 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('.navbar-nav .nav-item a').forEach(function(navLink) {
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('.navbar-nav .nav-item a').forEach(function (navLink) {
         var img = navLink.querySelector('img');
         var originalSrc = img.getAttribute('src');
         var hoverSrc = img.getAttribute('data-hover');
@@ -8,17 +8,18 @@ document.addEventListener('DOMContentLoaded', function() {
         img.setAttribute('src', navLink.classList.contains('active') ? hoverSrc : originalSrc);
 
         // Mouse enter event
-        navLink.addEventListener('mouseenter', function() {
+        navLink.addEventListener('mouseenter', function () {
             if (!navLink.classList.contains('active')) {
                 img.setAttribute('src', hoverSrc);
             }
         });
 
         // Mouse leave event
-        navLink.addEventListener('mouseleave', function() {
+        navLink.addEventListener('mouseleave', function () {
             if (!navLink.classList.contains('active')) {
                 img.setAttribute('src', originalSrc);
             }
         });
     });
 });
+
